9196f9c27ea9022f7b3c67ab1f7d2dba8009a3bf
Leonid Vinikov <czf.leo123@gmail.com>
2018-10-22 11:57:59 +0300

    Update okCupid.js

--- okCupid.js
+++ okCupid.js
@@ -13,3 +13,31 @@ async function likeMe(amount, waitTime){
     
 }
 likeMe(500, 3000)
+
+
+//copy and paste this into a javascript runner
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function likeMe(amount, waitTime, mod = 3) {
+    for (let i = 0; i < amount; i++) {
+        await sleep(waitTime);
+
+        if (mod % 3 == 0) {
+            console.log(document.getElementsByClassName("cardactions-action--like")[0]);
+            document.getElementsByClassName("cardactions-action--like")[0].click();
+
+            console.log("liking" + i + " times");
+        } else {
+            console.log(document.getElementsByClassName("cardactions-action--reject")[0]);
+            document.getElementsByClassName("cardactions-action--reject")[0].click();
+
+            console.log("rejecting" + i + " times");
+        }
+    }
+
+}
+
+likeMe(500, 3000, 3);
